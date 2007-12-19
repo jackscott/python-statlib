bfabac00f6fce906c42968eba15ccdacb3e74e83
istvan.albert <istvan.albert@6f1e4a0c-9f40-0410-87a2-617897439bad>
2007-12-19 14:59:31 +0000

    reindented to 4 spaces, added unittest sample

---
+++ test/test_all.py
@@ -0,0 +1,47 @@
+import sys, unittest
+from statlib import stats, pstat
+
+try:
+    from numpy import array as num_array
+except ImportError:
+    # numpy not installed
+    sys.stderr.write('Numpy not installed ... skipping numpy tests\n')
+    def num_array( values ):
+        return values
+
+class TestStatlib(unittest.TestCase):
+    
+    def setUp(self):
+        "Gets called on each test"
+        # generate list data
+        self.L  = range( 1, 21 )
+        self.LF = range( 1, 21 )
+        self.LF[2] = 3.0
+        self.LL = [ self.L ]*5
+
+        # will test array data if numpy is installed
+        self.A  = num_array( self.L )
+        self.AF = num_array( self.LF )
+        self.AA = num_array( self.LL )
+        
+        self.EQ = self.assertAlmostEqual
+
+    def test_geometricmean(self):
+        "Testing geometric mean"
+        data = [ self.L, self.LF, self.A, self.AF  ]
+        for datum in data :
+            self.EQ( stats.geometricmean( datum ), 8.304, 3)
+
+    def test_harmonicmean(self):
+        "Testing harmonic mean"
+        data = [ self.L, self.LF, self.A, self.AF  ]
+        for datum in data :
+            self.EQ( stats.harmonicmean( datum ), 5.559, 3)
+
+def get_suite():
+    suite = unittest.TestLoader().loadTestsFromTestCase( TestStatlib )
+    return suite
+
+if __name__ == '__main__':
+    suite = get_suite()
+    unittest.TextTestRunner(verbosity=2).run(suite)
